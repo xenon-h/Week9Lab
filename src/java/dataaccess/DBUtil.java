ec461da91d8610af8e64d69998be277747dd5ecf
ItsJordeh <ItsJordeh@gmail.com>
2020-10-29 10:02:09 -0600

    Added getAll method to UserDB class to read data from the database

--- src/java/dataaccess/DBUtil.java
+++ src/java/dataaccess/DBUtil.java
@@ -5,10 +5,33 @@
  */
 package dataaccess;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author 814992
  */
 public class DBUtil {
-    
+    public static void closePreparedStatement(Statement ps) {
+        try {
+            if (ps != null) {
+                ps.close();
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
 }
--- src/java/dataaccess/UserDB.java
+++ src/java/dataaccess/UserDB.java
@@ -5,10 +5,68 @@
  */
 package dataaccess;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import models.User;
+
 /**
  *
  * @author 814992
  */
 public class UserDB {
+    
+    ArrayList<User> userList = new ArrayList();
+    ConnectionPool cp = ConnectionPool.getInstance();
+    Connection con = cp.getConnection();
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    
+    String getAllStmt = "SELECT * from userdb";
+    
+    
+    
+    public ArrayList<User> getAll() throws Exception{
+        
+        
+        
+        try{
+        ps = con.prepareStatement(getAllStmt);
+        rs = ps.executeQuery();
+        
+        while(rs.next()){
+            
+            //email,active,firstname, lastname,password, role
+            
+            String email = rs.getString(1);
+            Boolean active = rs.getBoolean(2);
+            String firstName = rs.getString(3);
+            String lastName = rs.getString(4);
+            String password = rs.getString(5);
+            int role = rs.getInt(6);
+            
+            User user = new User(email, firstName,lastName, password, role, active);
+            userList.add(user);
+            
+            
+        }
+        
+        
+        
+        
+        }
+        finally {
+            DBUtil.closeResultSet(rs);
+            DBUtil.closePreparedStatement(ps);
+            cp.freeConnection(con);
+        }
+        return userList;
+        
+    }
+    
+    
+    
+    
     
 }
